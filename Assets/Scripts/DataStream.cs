5ff13203ba654e1f3cae612b3b6eaea14a747272
Alex Lementuev <a.lementuev@gmail.com>
2016-07-03 13:29:50 -0700

    Added test project dependency

---
+++ Assets/Scripts/DataStream.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+public class DataStream
+{
+    public T readStruct<T>(Type type) where T : struct
+    {
+        T t = new T();
+
+        FieldInfo[] fields = type.GetFields();
+
+        return t;
+    }
+}
