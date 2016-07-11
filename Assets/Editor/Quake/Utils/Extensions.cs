7dfb9e68ffecf9c4c7bfaa24b0e36fec65a5dfd6
Alex Lementuev <a.lementuev@gmail.com>
2016-07-10 21:38:56 -0700

    Progress

--- Assets/Editor/Quake/Utils/Extensions.cs
+++ Assets/Editor/Quake/Utils/Extensions.cs
@@ -24,6 +24,31 @@ public static class Extension
 
     #endregion
 
+    #region Type
+
+    public static T GetCustomAttribute<T>(this Type type, bool inherit = false) where T : Attribute
+    {
+        var attributes = type.GetCustomAttributes(typeof(T), inherit);
+        return attributes.Length == 1 ? attributes[0] as T : null;
+    }
+
+    public static T[] GetCustomAttributes<T>(this Type type, bool inherit = false) where T : Attribute
+    {
+        var attributes = type.GetCustomAttributes(typeof(T), inherit);
+        var result = new List<T>();
+        foreach (var attribute in attributes)
+        {
+            T t = attribute as T;
+            if (t != null)
+            {
+                result.Add(t);
+            }
+        }
+        return result.ToArray();
+    }
+
+    #endregion
+
     #region FieldInfo
 
     public static T GetCustomAttribute<T>(this FieldInfo field) where T : Attribute
