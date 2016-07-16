d9484f5dbeaf19e9cf4dd10b0c481db9aef8485c
Alex Lementuev <a.lementuev@gmail.com>
2016-07-16 14:52:20 -0700

    Progress

---
+++ Assets/Editor/Utils/AssetUtils.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+using System;
+using System.IO;
+using System.Collections;
+
+public static class AssetUtils
+{
+    static string s_projectPath;
+
+    public static bool AssetPathExists(string path)
+    {
+        if (!Path.IsPathRooted(path))
+        {
+            if (path.StartsWith("Assets/") || path.StartsWith("/Assets/"))
+            {
+                path = Path.Combine(projectPath, path);
+            }
+            else
+            {
+                throw new ArgumentException("Illegal asset path: " + path);
+            }   
+        }
+
+        return Directory.Exists(path) || File.Exists(path);
+    }
+
+    public static bool MakeDirectory(string path)
+    {
+        if (AssetPathExists(path))
+        {
+            return false;
+        }
+
+        if (Path.IsPathRooted(path))
+        {
+            path = UnityEditor.FileUtil.GetProjectRelativePath(path);
+        }
+        
+        var components = path.Split('/');
+        string parent = components[0];
+        if (parent != "Assets")
+        {
+            throw new ArgumentException("Illegal asset path: " + path);
+        }
+
+        for (int i = 1; i < components.Length; ++i) // first component is "Assets"
+        {
+            var subpath = parent + "/" + components[i];
+            if (!AssetPathExists(subpath))
+            {
+                AssetDatabase.CreateFolder(parent, components[i]);
+            }
+            parent = subpath;
+        }
+
+        return true;
+    }
+
+    public static string projectPath
+    {
+        get
+        {
+            if (s_projectPath == null)
+            {
+                s_projectPath = new DirectoryInfo(Application.dataPath).Parent.ToString();
+            }
+            return s_projectPath;
+        }
+    }
+
+}
