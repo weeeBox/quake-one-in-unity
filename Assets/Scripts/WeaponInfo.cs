f7c68aec58386e5f25180bbfd307cbed6a6494b6
Alex Lementuev <a.lementuev@gmail.com>
2016-07-16 17:32:15 -0700

    Progress

---
+++ Assets/Scripts/WeaponInfo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu]
+public class WeaponInfo : ScriptableObject
+{
+    [SerializeField]
+    Mesh m_mesh;
+
+    [SerializeField]
+    Material m_material;
+
+    [SerializeField]
+    QModelAnimation m_animation;
+
+    public Mesh mesh
+    {
+        get { return m_mesh; }
+    }
+
+    public Material material
+    {
+        get { return m_material; }
+    }
+
+    public QModelAnimation animation
+    {
+        get { return m_animation; }
+    }
+}
