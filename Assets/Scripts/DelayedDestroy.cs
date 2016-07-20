7d9d43e83097024dba76ade02ff6ada3cd79d1ec
Alex Lementuev <a.lementuev@gmail.com>
2016-07-19 19:50:55 -0700

    Added weapon shooting

---
+++ Assets/Scripts/DelayedDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedDestroy : MonoBehaviour {
+
+    [SerializeField]
+    float m_delay = 1.0f;
+
+	void Start ()
+    {
+        Destroy(gameObject, m_delay);
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum WeaponType
+{
+    Axe,
+    Shotgun,
+    SuperShotgun,
+    Nailgun,
+    SuperNailgun,
+    GrenadeLauncher,
+    RockerLauncher,
+    Lightning
+}
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    WeaponType m_weaponType;
+
+    [SerializeField]
+    Camera m_weaponCamera;
+
+    [SerializeField]
+    Transform m_shootingOrigin;
+
+    [SerializeField]
+    GameObject m_hit;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }   
+    }
+
+    public void Shoot()
+    {
+        Ray ray = m_weaponCamera.ViewportPointToRay(new Vector2(0.5f, 0.5f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Instantiate(m_hit, hit.point, Quaternion.identity);
+        }
+    }
+
+    public WeaponType weaponType
+    {
+        get { return m_weaponType; }
+    }
+}
