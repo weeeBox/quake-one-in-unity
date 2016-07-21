5ce0d720fae33d971f6e7f4b08bd639c730beeb3
Alex Lementuev <a.lementuev@gmail.com>
2016-07-20 22:55:01 -0700

    Added "hurt" and "death" animations to "dog" monster

--- Assets/Scripts/Entities/monster_dog.cs
+++ Assets/Scripts/Entities/monster_dog.cs
@@ -3,6 +3,29 @@ using System;
 using UnityEngine;
 
 // [RequireComponent(typeof())]
-public class monster_dog : entity
+public class monster_dog : monster_entity
 {
+    static readonly string[] kPainAnimations =
+    {
+        "pain_animation", "painb_animation"
+    };
+
+    static readonly string[] kDeathAnimations =
+    {
+        "death_animation", "deathb_animation"
+    };
+
+    #region Damage
+
+    protected override string[] DeathAnimations
+    {
+        get { return kDeathAnimations; }
+    }
+
+    protected override string[] PainAnimations
+    {
+        get { return kPainAnimations; }
+    }
+
+    #endregion
 }
